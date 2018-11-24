0955aed548117c6568a964c4aa0930f21f36c8ee
Derrick Yoo <derrickkyoo@gmail.com>
2018-11-24 15:20:32 -0700

    add basic request and secure secret keys in .env

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('Starting app');
+
+const dotenv = require('dotenv');
+const request = require('request');
+
+dotenv.load();
+
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY
+const requestURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_MAPS_API_KEY}&address=Tucson` 
+
+request({
+  url: requestURL,
+  json: true
+}, (error, response, body) => {
+  console.log(body);
+});
