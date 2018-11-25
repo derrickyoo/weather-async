fe0e25c8500a56febf88ace0ac36d7ed6b271caa
Derrick Yoo <derrickkyoo@gmail.com>
2018-11-24 22:05:35 -0700

    update app.js to take in user input address and query google maps api

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 console.log('Starting app');
 
 const dotenv = require('dotenv');
+const yargs = require('yargs');
 const request = require('request');
 
 dotenv.load();
 
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY
-const requestURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_MAPS_API_KEY}&address=Tucson` 
+
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
+
+let encodedAddress = encodeURIComponent(argv['address']);
+console.log(encodedAddress);
+
+let requestURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_MAPS_API_KEY}&address=${encodedAddress}`; 
 
 request({
   url: requestURL,
