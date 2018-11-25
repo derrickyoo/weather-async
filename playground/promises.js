793f2220ab14fda10d3d82cfedd1e382b76f8f0a
Derrick Yoo <derrickkyoo@gmail.com>
2018-11-25 15:00:07 -0700

    example promises in plaground dir

---
+++ playground/promises.js
@@ -0,0 +1,32 @@
+
+// Example 1
+let somePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('hello')
+  }, 2000)
+});
+
+somePromise.then((msg) => {
+  console.log(msg);
+}).catch((errorMsg) => {
+  console.log(errorMsg);
+});
+
+// Example 2
+let asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 2000);
+  })
+}
+
+asyncAdd(2, 3).then((res) => {
+  console.log(res);
+}, (errorMsg) => {
+  console.log(errorMsg)
+});
