011d5bcac05c4fc6468812fa434116539a81e988
Derrick Yoo <derrickkyoo@gmail.com>
2018-11-24 17:15:10 -0700

    simulate json response from google maps api

--- app.js
+++ app.js
@@ -6,11 +6,28 @@ const request = require('request');
 dotenv.load();
 
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY
+const environment = process.env.NODE_ENV
 const requestURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_MAPS_API_KEY}&address=Tucson` 
 
-request({
-  url: requestURL,
-  json: true
-}, (error, response, body) => {
-  console.log(body);
-});
+if (environment !== 'production') {
+  console.log("Running development environment")
+  let getGeo = (callback) => {
+    let data = require('./googleapis-example.json');
+
+    setTimeout(() => {
+      callback(data);
+    }, 1000);
+  }
+
+  getGeo((data) => {
+    console.log(JSON.stringify(data, undefined, 2));
+  });
+} else {
+  console.log("Running production environment")
+  request({
+    url: requestURL,
+    json: true
+  }, (error, response, body) => {
+    console.log(JSON.stringify(body, undefined, 2));
+  });
+}
