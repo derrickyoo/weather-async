95c9a860a536b18f491a66a954f75385f7b494d1
Derrick Yoo <derrickkyoo@gmail.com>
2018-11-24 20:17:50 -0700

    setup yargs for user input address

--- app-dev.js
+++ app-dev.js
@@ -1,7 +1,27 @@
 console.log('Starting app in development mode');
 
+const yargs = require('yargs');
 const exampleData = require('./googleapis-example.json');
 
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv);
+
+let encodedAddress = encodeURIComponent(argv['address']);
+console.log(encodedAddress);
+
+// Using setTimeout to mimic an async API request
 let getGeo = (callback) => {
   let body = exampleData;
 
@@ -11,6 +31,7 @@ let getGeo = (callback) => {
 }
 
 getGeo((body) => {
+  // Prettify the body for readability
   // console.log(JSON.stringify(body, undefined, 2));
   let results = body['results'][0];
 
